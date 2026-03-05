ff62e67841b20fc2f66e80c1a36f048fd571bb46
Peluas9000 <ayoubbenjaddi5@gmail.com>
2026-03-05 15:33:06 +0100

    Creado los dos objetos tipos bird y dog

--- typescript/ejercicio1.ts
+++ typescript/ejercicio1.ts
@@ -12,3 +12,20 @@ interface Dog extends Animal {
   race: string;
   age: number;
 }
+
+const bird: Bird = {
+  name: "Paloma",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: true,
+};
+
+const dog: Dog = {
+  name: "Rocky",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  race: "Labrador",
+  age: 5,
+};
